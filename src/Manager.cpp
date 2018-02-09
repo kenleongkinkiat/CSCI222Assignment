cd90dd57551176ca284e033ab0c81f769a6e15e0
huii <sihui@Sihuis-MacBook-Pro.local>
2018-02-09 18:02:20 +0800

    update

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -57,5 +57,65 @@ Manager::Manager(string username, string password, int no, string accStatus)
 void Manager::readData()
 {
     // read file and store into vector.
+    // read stockdata from .txt into stockRcds
+    ifstream infile(fileName);
+    
+    if(!infile)
+    {
+        cout << fileName << " error opening" << endl << endl;
+    }
+    
+    string line;
+    int i = 0;
+    
+    if(infile.is_open())
+    {
+      while(infile.good())
+      {
+          getline(infile,line);
+          Manager m = splitString(line);
+
+          ManagersRcd.push_back(m);
+          i++;
+      }
+    }
+
+    infile.close();
+
+}
+
+Manager Manager::splitString(string line)
+{
+
+    string username1,password1,unsuccessful,status;
+
+    istringstream buf(line);
+    for(string user; getline(buf,user, ',');)
+    {
+        // username
+        username = line;
+
+        for(string pass; getline(buf,pass, ',');)
+        {
+            password1 =  pass;
+            for(string s; getline(buf,s, ',');)
+            {
+              unsuccessful =  s;
+              for(string acc; getline(buf,acc, ',');)
+              {
+                status = acc;
+              }
+            }
+        }
+
+    } // end split
+
+    return new Manager(username1,password1,unsuccessful,status);
+
+}
+
+void Manager::verifyManager()
+{
+
 }
 
